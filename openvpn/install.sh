2c40961d19dfa685984abde6e69f779558a536ad
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-07-05 19:07:24 +0700

    x

--- openvpn/install.sh
+++ openvpn/install.sh
@@ -64,7 +64,7 @@ echo -e "[ ${BGreen}ok${NC} ] Restarting vnstat "
 clear
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                  : 22"  | tee -a log-install.txt
-echo "   - OpenVPN                  : 1194"  | tee -a log-install.txt
+echo "   - OpenVPN                  : TCP 1194"  | tee -a log-install.txt
 echo "   - Squid                    : 3128, 8000"  | tee -a log-install.txt
 echo "   - SSH Websocket            : 80" | tee -a log-install.txt
 echo "   - SSH SSL Websocket        : 443" | tee -a log-install.txt
